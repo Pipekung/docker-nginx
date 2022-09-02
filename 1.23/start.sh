59a6d5e373c435771fc2f4184b7f85cfcbb62f23
PK <cj.pipekung@gmail.com>
2022-09-02 08:46:00 +0700

    add 1.23

---
+++ 1.23/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# vim:sw=4:ts=4:et
+
+set -e
+
+echo "ROOT_DIR: $ROOT_DIR"
+echo "FASTCGI_PASS: $FASTCGI_PASS"
+
+sed -i 's|{{ROOT_DIR}}|$ROOT_DIR|g' /etc/nginx/conf.d/default.conf
+sed -i 's|{{FASTCGI_PASS}}|$FASTCGI_PASS|g' /etc/nginx/conf.d/default.conf
+
+exit 0
+# ./docker-entrypoint.sh
